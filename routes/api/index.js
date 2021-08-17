b2e57b8e441a921f3339afb1831f397a3cfd7f42
Rabeea Jahangir <rabeeajahangir@Rabeeas-MacBook-Air.local>
2021-08-16 21:45:30 -0400

    changes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
-
 // add prefix of `/users` to routes created in `user-routes.js`
 router.use('/users', pizzaRoutes);
 
