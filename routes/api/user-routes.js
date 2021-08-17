d5e535575537c905e5a89e24c7ed296f83c0f51e
Rabeea Jahangir <rabeeajahangir@Rabeeas-MacBook-Air.local>
2021-08-16 22:28:52 -0400

    added thought routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+
+
+const {
+    getAllUser,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+  } = require('../../controllers/user-controller');
+
+// Set up GET all and POST at /api/users
+// /api/users
+router
+  .route('/')
+  .get(getAllUsers)
+  .post(createUsers);
+
+// Set up GET one, PUT, and DELETE at /api/userss/:id
+// /api/users/:id
+router
+  .route('/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
+
+  router
+.route('/:userId/friends/:friendId')
+.post(addFriend)
+.put(removeFriend)
+module.exports = router;
+
+
+
+
+
+
+
+
+module.exports = router;
